ff5dca21deb022031be0c5ffee5274fe5a8381c8
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-08-25 11:32:07 -0700

    Add URL-For

--- main.py
+++ main.py
@@ -1,14 +1,56 @@
 from flask import Flask, render_template, url_for
 
+
 myapp=Flask(__name__)
 
 @myapp.route("/")
 def main():
     return render_template("index.html")
 
-@myapp.route("/home.html")
+@myapp.route("/home")
 def home():
     return render_template("home.html")
+
+@myapp.route("/zg2win")
+def zg2win():
+    return render_template("zg2win.html")
+
+@myapp.route("/uni2zg")
+def uni2zg():
+    return render_template("uni2zg.html")
+
+@myapp.route("/uni2win")
+def uni2win():
+    return render_template("uni2win.html")
+
+@myapp.route("/win2zg")
+def winzg():
+    return render_template("winzg.html")
+
+@myapp.route("/win2uni")
+def win2uni():
+    return render_template("win2uni.html")
+
+@myapp.route("/partner")
+def partner():
+    return render_template("partner.html")
+
+@myapp.route("/question & answer")
+def question & answer():
+    return render_template("question & answer.html")
+
+@myapp.route("/resources")
+def resources():
+    return render_template("resources.html")
+
+@myapp.route("/tools")
+def tools():
+    return render_template("tools.html")
+
+@myapp.route("/work")
+def work():
+    return render_template("work.html")
+
 
 if __name__=="__main__":
     myapp.run(debug=True)
