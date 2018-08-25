c70dedc7ab4450065810ac443aa1b10c2d6567ab
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-08-25 08:10:34 -0700

    Fixed home

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ myapp=Flask(__name__)
 @myapp.route("/")
 def main():
     return render_template("index.html")
+
+@myapp.route("/home")
+def home():
+    return render_template("home.html")
+
 if __name__=="__main__":
     myapp.run(debug=True)
